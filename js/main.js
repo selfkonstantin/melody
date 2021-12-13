87130f2cdfc6871b474e138e694ce657b66ec071
Konstantin <self.konstantin@gmail.com>
2021-12-13 12:30:45 +0500

    Step 5

--- js/main.js
+++ js/main.js
@@ -1,14 +1,21 @@
 $(document).ready(function () {
   var currentFloor = 2;
-  var counterUp = $('.counter-up')
-  var counterDown = $('.counter-down')
-  var floorPath = $('.home-image path')
+  var counterUp = $('.counter-up');
+  var counterDown = $('.counter-down');
+  var floorPath = $('.home-image path');
+  var modal = $('.modal');
+  var modalCloseButton = $('.modal-close-button');
+  var viewFlatsButton = $('.view-flats')
 
   floorPath.on('mouseover', function() {
     floorPath.removeClass('current-floor');
     currentFloor = $(this).attr('data-floor')
     $('.counter').text(currentFloor)
   });
+
+  floorPath.on('click', toggleModal);
+  modalCloseButton.on('click', toggleModal);
+  viewFlatsButton.on('click', toggleModal);
 
   counterUp.on('click', function() {
     if (currentFloor < 18) {
@@ -18,8 +25,8 @@ $(document).ready(function () {
       floorPath.removeClass('current-floor');
       $(`[data-floor=${usCurrentFloor}]`).toggleClass('current-floor');
     }
-  })
-  
+  });
+
   counterDown.on('click', function() {
     if (currentFloor > 2) {
       currentFloor--
@@ -28,5 +35,9 @@ $(document).ready(function () {
       floorPath.removeClass('current-floor');
       $(`[data-floor=${usCurrentFloor}]`).toggleClass('current-floor');
     }
-  })
+  });
+
+  function toggleModal() {
+    modal.toggleClass('is-open');
+  };
 });
